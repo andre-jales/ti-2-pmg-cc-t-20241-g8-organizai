a04e84a48ae0dd0cfb471b22d31db62189537dfa
andre-jales <ajales@sga.pucminas.br>
2024-06-21 00:23:17 -0300

    Atualizando

--- Codigo/src/main/java/dao/UsuarioDAO.java
+++ Codigo/src/main/java/dao/UsuarioDAO.java
@@ -65,7 +65,7 @@ public class UsuarioDAO {
 		try {
 			PreparedStatement ps = conn.prepareStatement("INSERT INTO public.usuario(email, senhahash, nome) VALUES (?, ?, ?)");
 			ps.setString(1, usuario.getEmail());
-			ps.setString(2, argon2.hash(10, 65536, 1, usuario.getSenha()));
+			ps.setString(2, argon2.hash(10, 65536, 1, usuario.getSenha().toCharArray()));
 			ps.setString(3, usuario.getNome());
 			
 			int rowsInserted = ps.executeUpdate();
@@ -167,7 +167,7 @@ public class UsuarioDAO {
 			
 			if(rs.next()) {
 				// if(usuarioLogin.getSenha().equals(rs.getString("senhahash")))
-				if(argon2.verify(rs.getString("senhahash"), usuarioLogin.getSenha()))
+				if(argon2.verify(rs.getString("senhahash"), usuarioLogin.getSenha().toCharArray()))
 				{
 					usuario = new Usuario(rs.getInt("userid"),
 					null, 
